09edd3db6c406e7719c2d4bde42177c80dd3728b
Jose Humire <jhumireh@unsa.edu.pe>
2020-11-27 17:40:25 -0500

    Modelos creados y django admin preparado

--- work_day/admin.py
+++ work_day/admin.py
@@ -1,3 +1,58 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+
+@admin.register(Country)
+class CountryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'creation_date')
+
+
+@admin.register(City)
+class CityAdmin(admin.ModelAdmin):
+    list_display = ('country', 'name')
+
+
+@admin.register(School)
+class SchoolAdmin(admin.ModelAdmin):
+    list_display = ('city', 'name')
+
+
+@admin.register(Profession)
+class ProfessionAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')
+
+
+@admin.register(Professional)
+class ProfessionalAdmin(admin.ModelAdmin):
+    list_display = ('user', 'city', 'phone', 'id', 'status', 'creation_date')
+
+
+@admin.register(Curriculum)
+class CurriculumAdmin(admin.ModelAdmin):
+    list_display = ('owner', 'contract_price', 'score')
+
+
+@admin.register(Job)
+class JobAdmin(admin.ModelAdmin):
+    list_display = ('cv', 'profession', 'description', 'start_date', 'finish_date')
+
+
+@admin.register(JobOffer)
+class JobOfferAdmin(admin.ModelAdmin):
+    list_display = ('user', 'city', 'description', 'creation_date', 'status')
+
+
+@admin.register(Employment)
+class EmploymentAdmin(admin.ModelAdmin):
+    list_display = ('offer', 'profession', 'description', 'reward', 'status')
+
+
+@admin.register(ChatRoom)
+class ChatRoomAdmin(admin.ModelAdmin):
+    list_display = ('creation_date', 'status')
+
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('chat_room', 'user', 'message', 'creation_date')
--- work_day/models.py
+++ work_day/models.py
@@ -1,9 +1,96 @@
+import datetime
+
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
+
+
+class Country(models.Model):
+    name = models.CharField(max_length=40)
+    creation_date = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.name
+
+
+class City(models.Model):
+    country = models.ForeignKey(Country, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class School(models.Model):
+    city = models.ForeignKey(City, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+class Profession(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=100, default='')
+
+    def __str__(self):
+        return self.name
 
 
 class Professional(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    description = models.TextField()
+    city = models.ForeignKey(City, on_delete=models.CASCADE, default='')
+    professions = models.ManyToManyField(Profession)
+    phone = models.CharField(max_length=9, default='')
+    id_number = models.CharField(max_length=11, default='')
+    status = models.BooleanField(default=True)
+    creation_date = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.user.get_username()
 
 
+class Curriculum(models.Model):
+    owner = models.OneToOneField(Professional, on_delete=models.CASCADE)
+    contract_price = models.IntegerField(default=0)
+    score = models.IntegerField(default=5)
+
+
+class Job(models.Model):
+    cv = models.ForeignKey(Curriculum, on_delete=models.CASCADE)
+    profession = models.OneToOneField(Profession, on_delete=models.CASCADE)
+    description = models.TextField(max_length=500, default='')
+    start_date = models.DateTimeField(default=timezone.now)
+    finish_date = models.DateTimeField(default=timezone.now() + datetime.timedelta(days=1))
+
+    def __str__(self):
+        return self.profession.name
+
+
+class JobOffer(models.Model):
+    user = models.ForeignKey(Professional, on_delete=models.CASCADE)
+    city = models.ForeignKey(City, on_delete=models.CASCADE, default='')
+    description = models.CharField(max_length=100, default='')
+    creation_date = models.DateTimeField(default=timezone.now)
+    status = models.BooleanField(default=True)
+
+
+class Employment(models.Model):
+    offer = models.ForeignKey(JobOffer, on_delete=models.CASCADE)
+    profession = models.OneToOneField(Profession, on_delete=models.CASCADE)
+    description = models.TextField(max_length=200, default='')
+    reward = models.FloatField(default=0)
+    status = models.BooleanField(default=True)
+
+
+class ChatRoom(models.Model):
+    users = models.ManyToManyField(Professional)
+    creation_date = models.DateTimeField(default=timezone.now)
+    status = models.BooleanField(default=True)
+
+
+class Message(models.Model):
+    chat_room = models.ForeignKey(ChatRoom, on_delete=models.CASCADE)
+    user = models.ForeignKey(Professional, on_delete=models.CASCADE)
+    message = models.CharField(max_length=400)
+    creation_date = models.DateTimeField(default=timezone.now)
