4c2f14d7e6f8f7266dc3d2df4804e0a88a2bfbdc
Nur Ali <nali@unsa.edu.pe>
2020-12-06 18:01:08 -0500

    Vista de ofertas de trabajo

--- work_day/views.py
+++ work_day/views.py
@@ -164,3 +164,13 @@ def create_job_offer(request):
             'employment_formset': employment_formset
         }
     )
+
+
+def job_offer(request, offer_id):
+    offer_list = JobOffer.objects.all()
+    current_offer = JobOffer.objects.get(pk=offer_id)
+    context = {
+        'offers': offer_list,
+        'offer': current_offer,
+    }
+    return render(request, 'offers.html', context)
