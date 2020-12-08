4820b2a60f88dcb51539b5f982e8ee65dfa1d408
Nur Ali <nali@unsa.edu.pe>
2020-12-08 12:38:00 -0500

    Recuperar contraseña

--- proyecto_cs/urls.py
+++ proyecto_cs/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import path
 from work_day import views
 
@@ -39,4 +40,9 @@ urlpatterns = [
 
     path('jobOffers/<int:offer_id>', views.job_offer, name='job_offer'),
     path('my_posts', views.my_posts),
+
+    path('resetPassword/', auth_views.PasswordResetView.as_view(), name='reset_password'),
+    path('resetPasswordSent/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('resetPasswordComplete/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
