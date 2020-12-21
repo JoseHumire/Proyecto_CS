3ba705cd1fad29a03eb41d03717bff5cf20f6184
JoseHumire <jhumireh@unsa.edu.pe>
2020-12-20 23:53:02 -0500

    Unit tests de vistas v1

--- work_day/tests/unit_tests/test_views.py
+++ work_day/tests/unit_tests/test_views.py
@@ -17,11 +17,12 @@ class BaseTest(TestCase):
         user = User.objects.create_user(
             username='testUser', password='123456'
         )
-        country = Country.objects.create(name='Peru')
-        city = City.objects.create(country=country, name='Arequipa')
-        Professional.objects.create(
-            user=user, city=city, phone='999999999', id_number='11111111',
+        self.country = Country.objects.create(name='Peru')
+        self.city = City.objects.create(country=self.country, name='Arequipa')
+        self.professional = Professional.objects.create(
+            user=user, city=self.city, phone='999999999', id_number='11111111',
         )
+        cv = Curriculum.objects.create(owner=self.professional)
         return super().setUp()
 
 
@@ -58,17 +59,49 @@ class RegisterTest(BaseTest):
 
 
 class ViewsTest(BaseTest):
+    def test_index_view_doesnt_require_login(self):
+        response = self.client.get(reverse('index'), follow=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'index.html')
+
     def test_home_view_without_login(self):
-        response = self.client.get(
-            reverse('home'), follow=True
-        )
+        response = self.client.get(reverse('home'), follow=True)
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'users/login.html')
 
     def test_home_view_with_login(self):
         self.client.login(username='testUser', password='123456')
-        response = self.client.get(
-            reverse('home'), follow=True
-        )
+        response = self.client.get(reverse('home'), follow=True)
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'home.html')
+
+    def test_view_user_profile_without_login(self):
+        response = self.client.get(reverse('user_profile'), follow=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'users/login.html')
+
+    def test_view_own_user_profile_with_login(self):
+        self.client.login(username='testUser', password='123456')
+        response = self.client.get(reverse('user_profile'), follow=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'users/profile.html')
+
+    def test_view_professionals_without_login(self):
+        response = self.client.get(reverse('professionals'), follow=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'users/login.html')
+
+    def test_view_professionals_with_login(self):
+        self.client.login(username='testUser', password='123456')
+        response = self.client.get(reverse('professionals'), follow=True)
+        print(response.context['professionals'][0])
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'professionals.html')
+        self.assertEqual(len(response.context['professionals']), 1)
+
+    def test_view_my_posts_receive_offers_list(self):
+        self.client.login(username='testUser', password='123456')
+        response = self.client.get(reverse('my_posts'), follow=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'my_posts.html')
+        self.assertEqual(len(response.context['offers']), 0)
